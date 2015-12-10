7f10e8d99361df6b346c94777052c8783e2fd2ea
Nick Ramirez <nramirez@aclens.local>
2015-12-10 14:03:00 -0500

    Fixed version for elasticsearch

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['logstash']['version'] = '2.1'
-default['elasticsearch']['version'] = '2.1'
+default['elasticsearch']['version'] = '2.x'
 
 # Overrides for java cookbook
 override['java']['install_flavor'] = 'openjdk'
