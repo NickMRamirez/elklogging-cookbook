1aac7f6ae71d2ee863419d77ac5ad15a73dc45e1
Nick Ramirez <nramirez@aclens.local>
2015-12-11 10:40:22 -0500

    Changing how installing kibana

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['logstash']['version'] = '2.1'
 default['elasticsearch']['version'] = '2.x'
+default['kibana']['version'] = '4.1'
 
 # Overrides for java cookbook
 override['java']['install_flavor'] = 'openjdk'
-override['java']['jdk_version'] = '7'
+override['java']['jdk_version'] = '8'
--- recipes/kibana.rb
+++ recipes/kibana.rb
@@ -1,23 +1,37 @@
-kibana_package = 'https://download.elastic.co/kibana/kibana/kibana-4.0.2-linux-x64.tar.gz'
-kibana_file = File.basename(kibana_package)
-kibana_local_file = "#{Chef::Config[:file_cache_path]}/#{kibana_file}"
 
-directory '/opt/kibana'
+version = node['kibana']['version']
 
-remote_file kibana_local_file do
-  source kibana_package
-  notifies :run, "execute[extract_file]", :immediately
+apt_repository 'kibana' do
+  uri "http://packages.elasticsearch.org/kibana/#{version}/debian"
+  key 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch'
+  components ['stable', 'main']
 end
 
-execute 'extract_file' do
-  command "tar xzf #{kibana_local_file} -C /opt/kibana --strip-components 1"
-  action :nothing
-end
+apt_package 'kibana'
 
-# file must have unix-style line endings
-cookbook_file '/etc/init.d/kibana' do
-  mode "700"
-end
+
+
+
+# kibana_package = 'https://download.elastic.co/kibana/kibana/kibana-4.0.2-linux-x64.tar.gz'
+# kibana_file = File.basename(kibana_package)
+# kibana_local_file = "#{Chef::Config[:file_cache_path]}/#{kibana_file}"
+
+# directory '/opt/kibana'
+
+# remote_file kibana_local_file do
+  # source kibana_package
+  # notifies :run, "execute[extract_file]", :immediately
+# end
+
+# execute 'extract_file' do
+  # command "tar xzf #{kibana_local_file} -C /opt/kibana --strip-components 1"
+  # action :nothing
+# end
+
+# # file must have unix-style line endings
+# cookbook_file '/etc/init.d/kibana' do
+  # mode "700"
+# end
 
 service 'kibana' do
   supports [ :status ]
